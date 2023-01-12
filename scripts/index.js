29cc045bb9d14b9d90481a7100543a718659e194
S-Sneha <098sneha@gmail.com>
2023-01-12 19:53:20 +0530

    Adding the task modal and task contents

--- scripts/index.js
+++ scripts/index.js
@@ -2,5 +2,132 @@
 
 const state = {
     taskList: [],
+};
+
+// dom manipulations
+const taskModal = document.querySelector(".task__modal__body");
+const taskContents = document.querySelector(".task__contents");
+
+// To create a card (task) on Home page
+const htmlTaskContent = ({ id, title, description, type, url }) => 
+` <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
+    <div class='card shadow-sm task__card'>
+      <div class='card-header d-flex gap-2 justify-content-end task__card__header'>
+        <button type='button' class='btn btn-outline-info mr-2' name=${id} onclick="editTask.apply(this, arguments)">
+          <i class='fas fa-pencil-alt' name=${id}></i>
+        </button>
+        <button type='button' class='btn btn-outline-danger mr-2' name=${id} onclick="deleteTask.apply(this, arguments)">
+          <i class='fas fa-trash-alt' name=${id}></i>
+        </button>
+      </div>
+      <div class='card-body'>
+        ${
+          url
+            ? `<img width='100%' height='150px' style="object-fit: cover; object-position: center"  src=${url} alt='card image' class='card-image-top md-3 rounded-lg' />`
+            : `<img width='100%' height='150px' style="object-fit: cover; object-position: center"  src="https://tse3.mm.bing.net/th?id=OIP.LZsJaVHEsECjt_hv1KrtbAHaHa&pid=Api&P=0" 
+               alt='card image' class='card-image-top md-3 rounded-lg' />`
+        }
+        <h4 class='task__card__title'>${title}</h4>
+        <p class='description trim-3-lines text-muted' data-gram_editor='false'>
+          ${description}
+        </p>
+        <div class='tags text-white d-flex flex-wrap'>
+          <span class='badge bg-primary m-1'>${type}</span>
+        </div>
+      </div>
+      <div class='card-footer'>
+        <button 
+        type='button' 
+        class='btn btn-outline-primary float-right' 
+        data-bs-toggle='modal'
+        data-bs-target='#showTask'
+        id=${id}
+        onclick='openTask.apply(this, arguments)'>
+          Open Task
+        </button>
+      </div>
+    </div>
+  </div>
+`;
+
+// Dynamic modals or cards on our home page or user-interface
+const htmlModalContent = ({ id, title, description, url }) => {
+    const date = new Date(parseInt(id));
+    return `
+  <div id = ${id}>
+  ${
+    url
+      ? `<img width='100%' src=${url} alt='card image here  class='img-fluid place__holder__image mb-3'/>`
+      : `<img width='100%' height='150px' style="object-fit: cover; object-position: center"  src="https://tse3.mm.bing.net/th?id=OIP.LZsJaVHEsECjt_hv1KrtbAHaHa&pid=Api&P=0" alt='card image cap' class='card-image-top md-3 rounded-lg' />`
+  }
+  <strong class="text-sm text-muted">Created on ${date.toDateString()}</strong>
+  <h2 class="my-3">${title}</h2>
+  <p class='lead'>${description}</p>
+  </div>`;
   };
+
+  // Updating our local storage (the modals/cards which we see on our user-interface)
+const updateLocalStorage = () => {
+    localStorage.setItem(
+      "task",
+      JSON.stringify({
+        tasks: state.taskList,
+      })
+    );
+  };
+
+//To get the cards or modals on the UI from local storage (Browsers storage)
+const loadInitialData = () => {
+    const localStorageCopy = JSON.parse(localStorage.task);
   
+    if (localStorageCopy) state.taskList = localStorageCopy.tasks;
+  
+    state.taskList.map((cardDate) => {
+      taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardDate));
+    });
+  };
+
+  //Submit function : handleSubmit()
+  const handleSubmit = (event) => {
+    const id = `${Date.now()}`;
+    const input = {
+      url: document.getElementById("imageUrl").value,
+      title: document.getElementById("taskTitle").value,
+      type: document.getElementById("tags").value,
+      description: document.getElementById("taskDescription").value,
+    };
+    if (input.title === "" || input.type === "" || input.description === "") {
+      return alert("Please fill all the fields");
+    }
+    taskContents.insertAdjacentHTML(
+      "beforeend",
+      htmlTaskContent({
+        ...input,
+        id,
+      })
+    );
+    // updated task list - for first go
+    state.taskList.push({ ...input, id });
+
+     // update the same on localStorage 
+     updateLocalStorage();
+};
+
+
+// Opens new modal on our UI when user clicks open task : openTask()
+const openTask = (e) => {
+  // pop up the current one
+  if (!e) e = window.event;
+
+  // find the correct card opened
+  const getTask = state.taskList.find(({ id }) => id === e.target.id);
+  taskModal.innerHTML = htmlModalContent(getTask);
+};
+
+//Delete operation : deleteTask()
+
+
+//Edit operation : editTask()
+
+
+
